7021090cfe588a9b1d612c559b51cdf67fbb0078
Gabriel Castro <gabriel.cgcr.cgcr@gmail.com>
2023-06-08 18:28:33 -0300

    feat: layout init

--- src/components/BestTransport.vue
+++ src/components/BestTransport.vue
@@ -6,19 +6,39 @@
         <b>{{ appName }}</b>
       </b-navbar-brand>
     </b-navbar>
-    <Main></Main>
+    <form>
+      <h1>Insira o destino e o peso</h1>
+      <!-- input de destino -->
+      <label for="cidade">Destino</label>
+      <select class="form-select" aria-label="Destino" id="destino">
+        <option selected>Selecione o destino</option>
+      </select>
+      <!-- input de peso -->
+      <label for="peso">Peso</label>
+      <input
+        type="text"
+        class="form-control"
+        id="peso"
+        placeholder="300 kg"
+        aria-label="Peso"
+      />
+      <!-- botao de analisar -->
+      <button type="button" class="btn btn-primary">Analisar</button>
+    </form>
+    <!-- saida dos dados -->
+    <div>
+      <h1>Nenhum dado selecionado</h1>
+    </div>
   </div>
 </template>
 
 <script>
 import { BNavbar, BNavbarBrand } from "bootstrap-vue";
-import Main from "./Main.vue";
 
 export default {
   components: {
     BNavbar,
     BNavbarBrand,
-    Main,
   },
   data() {
     const appName = "";
@@ -30,6 +50,7 @@ export default {
   created() {
     // Implemente aqui o GET dos dados da API REST
     // para que isso ocorra na inicialização da pagina
+
     this.appName = "Melhor Frete";
   },
   methods: {
@@ -38,6 +59,7 @@ export default {
       console.log(this.appName);
     },
   },
+  watch: {},
 };
 </script>
 
